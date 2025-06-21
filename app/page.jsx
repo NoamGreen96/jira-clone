857e126fc89b8eea594c02e5166404100e0fc505
unknown <noamgr11@gmail.com>
2025-06-21 22:41:22 +0300

    add carousel

---
+++ app/page.jsx
@@ -0,0 +1,85 @@
+import { ChevronRight, Layout, Calendar, BarChart } from "lucide-react";
+import { Button } from "../components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { Card, CardContent } from "../components/ui/card";
+import CompanyCarousel from "../components/company-carousel";
+
+const features = [
+  {
+    title: "Intuitive Kanban Boards",
+    description:
+      "Visualize your workflow and optimize team productivity with our easy-to-use Kanban boards.",
+    Icon: Layout,
+  },
+  {
+    title: "Powerful Sprint Planning",
+    description:
+      "Plan and manage sprints effectively, ensuring your team stays focused on delivering value.",
+    Icon: Calendar,
+  },
+  {
+    title: "Comprehensive Reporting",
+    description:
+      "Gain insights into your team's performance with detailed, customizable reports and analytics.",
+    Icon: BarChart,
+  },
+];
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      {/* {Hero Section} */}
+      <section className="container mx-auto py-20 text-center">
+        <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold gradient-title pb-6 flex flex-col ">
+          Streamline Your Workflow <br />
+          <span className="flex mx-auto gap-3 sm:gap-4 items-center">
+            with{" "}
+            <Image
+              src={"/logo2.png"}
+              alt="Zscrum Logo"
+              width={400}
+              height={80}
+              className="h-14 sm:h-24 w-auto object-contain"
+            />
+          </span>
+        </h1>
+        <p>Empower your team with our intutive project managment solution.</p>
+        <Link href='/onboarding'>
+          <Button size="lg" variant='secondary' className="mr-4">
+            Get Started
+            <ChevronRight size={18} className="ml-1" />
+          </Button>
+        </Link>
+        <Link href='#features'>
+          <Button size="lg" variant='outline' className="mr-4"> Learn More<ChevronRight size={18} className="ml-1" />
+          </Button>
+        </Link>
+      </section>
+      <section id="features" className="bg-gray-900 py-20 px-5">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">Key Features</h3>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+            {features.map((feature, index) => {
+              return (
+                <Card key={index}>
+                  <CardContent className='pt-6'>
+                    <feature.Icon className="h-12 w-12 mb-4 text-blue-300" />
+                    <h4 className="text-xl font-semibold mb-2">{feature.title}</h4>
+                    <p className="text-gray-300">{feature.description}</p>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+      <section className="py-20">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">Trusted by industry Leaders</h3>
+          <CompanyCarousel />
+        </div>
+      </section>
+    </div>
+  )
+}
