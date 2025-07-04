22aa177450828f5c420890210ebb1aa350d98e42
unknown <noamgr11@gmail.com>
2025-07-05 00:05:56 +0300

    clerk hotfix

--- app/layout.js
+++ app/layout.js
@@ -16,27 +16,30 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider appearance={{
-      baseTheme: shadesOfPurple,
-      variables: {
-        colorPrimary: "#3b82f6",
-        colorBackground: "#1a202c",
-        colorInputBackground: "#2D3748",
-        colorInputText: "#F3F4F6",
-      },
-      elements: {
-        formButtonPrimary: "text-white",
-        card: "bg-gray-800",
-        headerTitle: "text-blue-400",
-        headerSubtitle: "text-gray-400"
-      }
-    }}>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      appearance={{
+        baseTheme: shadesOfPurple,
+        variables: {
+          colorPrimary: "#3b82f6",
+          colorBackground: "#1a202c",
+          colorInputBackground: "#2D3748",
+          colorInputText: "#F3F4F6",
+        },
+        elements: {
+          formButtonPrimary: "text-white",
+          card: "bg-gray-800",
+          headerTitle: "text-blue-400",
+          headerSubtitle: "text-gray-400"
+        }
+      }}
+    >
       <html lang="en">
         <body className={`${inter.className} dotted-background`}>
           <ThemeProvider attribute="class" defaultTheme="dark">
             <Header />
             <main className="min-h-screen"> {children} </main>
-            <Toaster richColors/>
+            <Toaster richColors />
             <footer className="bg-gray-900 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
                 <p>Made with Love</p>
